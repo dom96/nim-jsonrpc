512ff16328b3c09ccbc116531c4f40fdecca9857
Dominik Picheta <dominikpicheta@googlemail.com>
2015-02-11 22:22:19 +0000

    More comments.

--- tests/clienttest.nim
+++ tests/clienttest.nim
@@ -1,9 +1,12 @@
 import jsonrpc/client, asyncdispatch, json
 
 proc main() {.async.} =
+  # Create a new instance of the server.
   var rpcClient = newAsyncRpcClient()
+  # Connect to the server.
   await rpcClient.connect("localhost", Port(5678))
 
+  # Call some procedures defined on the server.
   assert(await(rpcClient.call("add", %{"x": %123, "y": %500})).num == 623)
   assert(await(rpcClient.call("add", %{"x": %5, "y": %23})).num == 28)
 
--- tests/servertest.nim
+++ tests/servertest.nim
@@ -1,11 +1,14 @@
 import jsonrpc/server, asyncdispatch, json
 
 proc add(params: JsonNode): Future[JsonNode] {.async.} =
+  ## Simply adds two numbers together.
   assertParam(params, "x", JInt)
   assertParam(params, "y", JInt)
   return %(params["x"].num + params["y"].num)
 
 when isMainModule:
   var rpcServer = newAsyncRpcServer()
+  # Register the ``add`` procedure with the server instance.
   rpcServer.register("add", add)
+  # Listen for connections.
   waitFor rpcServer.serve(Port(5678))
